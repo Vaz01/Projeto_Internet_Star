e3e66f5b8fb05103a82787973b0f77e6fe10c392
Vaz01 <felipevaz727@gmail.com>
2026-01-18 19:11:07 -0300

    Finalização Simples

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,12 +1,14 @@
-const toggleBtn = document.querySelector(".toggle_btn")
-const toggleBtnIcon = document.querySelector(".toggle_btn i")
-const dropDownMenu = document.querySelector(".dropdown_menu")
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.querySelector(".toggle_btn");
+    const toggleBtnIcon = document.querySelector(".toggle_btn i");
+    const dropDownMenu = document.querySelector(".dropdown_menu");
 
-toggleBtn.onclick = () => {
-    dropDownMenu.classList.toggle("open")
+    toggleBtn.addEventListener("click", () => {
+        dropDownMenu.classList.toggle("open");
 
-    const isOpen = dropDownMenu.classList.contains("open")
+        const isOpen = dropDownMenu.classList.contains("open");
 
-    toggleBtnIcon.classList.remove("fa-bars", "fa-xmark")
-    toggleBtnIcon.classList.add(isOpen ? "fa-xmark" : "fa-bars")
-}
+        toggleBtnIcon.classList.remove("fa-bars", "fa-xmark");
+        toggleBtnIcon.classList.add(isOpen ? "fa-xmark" : "fa-bars");
+    });
+});
